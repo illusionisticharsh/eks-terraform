88bb6a5bfb1298f3029bf7d2d771c5211debad86
illusionisticharsh <harshstark1996@gmail.com>
2026-07-10 09:55:55 +0530

    variables updated

---
+++ eks/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {}
+variable "env" {}
+variable "cluster-name" {}
+variable "vpc-cidr-block" {}
+variable "vpc-name" {}
+variable "igw-name" {}
+variable "pub-subnet-count" {}
+variable "pub-cidr-block" {
+  type = list(string)
+}
